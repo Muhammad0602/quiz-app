0676f77f0c44dc88bd594e1fc95cc4ec6442b841
Muhammad Davlatov <umedjon02.ud@gmail.com>
2024-01-19 22:42:16 +0500

    Add flashcards to the page for test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import FlashcardList from "./FlashcardList";
 
 function App() {
   return (
-    <h1>Hello Guys</h1>
+    <>
+    <FlashcardList flashcards={DATA_API} />
+    </>
   );
 }
+
+const DATA_API = [
+  {
+    id: 1,
+    question: "What is 3 + 3", 
+    options: [
+      "4",
+      "5",
+      "7",
+      "6"
+    ],
+    answer: "6"
+  },
+
+  {
+    id: 2,
+    question: "What is 4 + 6", 
+    options: [
+      "24",
+      "35",
+      "10",
+      "63"
+    ],
+    answer: "10"
+  },
+  
+]
 
 export default App;
---
+++ src/Flashcard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Flashcard({flashcard}) {
+  return (
+    <div>
+        {flashcard.question } =
+        {flashcard.answer}
+    </div>
+  )
+}
